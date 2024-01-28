5524e42f530f3a8b878acf12b0b513d31f7b6d85
Diiiii <diblagochinnova@gmail.com>
2024-01-29 01:20:41 +0200

    update footer.js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,25 +1,23 @@
 import axios from 'axios';
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('subscriptionForm');
-    const email = document.getElementById('email');
+const form = document.getElementById('subscriptionForm');
+const email = document.getElementById('email');
 
-    form.addEventListener('submit', async function (event) {
-        event.preventDefault();
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
 
-        if (!form.checkValidity()) {
-            alert('Please fill in a valid email address.');
-        } else {
-            const data = {email: email.value};
-            try {
-                const response = await axios.post('https://energyflow.b.goit.study/api-docs/#/Subscription', data);
-                
-                alert("We're excited to have you on board! 🎉 Thank you for subscribing to new exercises on Energy Flow. You've just taken a significant step towards improving your fitness and well-being.");
-                console.log('Server response:', response);
-            } catch (error) {
-                console.error('Error:', error);
-                alert('Something went wrong, try again');
-            }
+    if (!form.checkValidity()) {
+        alert('Please fill in a valid email address.');
+    } else {
+        const data = { email: email.value };
+        try {
+            const response = await axios.post('https://energyflow.b.goit.study/api-docs/#/Subscription', data);
+
+            alert("We're excited to have you on board! 🎉 Thank you for subscribing to new exercises on Energy Flow. You've just taken a significant step towards improving your fitness and well-being.");
+            console.log('Server response:', response);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Something went wrong, try again');
         }
-    });
+    }
 });
