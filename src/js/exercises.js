7d5e693fd23c11631be2f21fc796a014da48e2a9
Олег Слюсар <eternityonly1@gmail.com>
2024-01-29 11:49:04 +0200

    start js

---
+++ src/js/exercises.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const exercisesTitle = document.querySelector('.exercises-title');
+const exercisesFilterButton = document.querySelector(
+  '.exercises-filter-button'
+);
+const exercisesInput = document.querySelector('.exercises-input-wrapper');
+const exercisesContainer = document.querySelector('.exercises-container');
