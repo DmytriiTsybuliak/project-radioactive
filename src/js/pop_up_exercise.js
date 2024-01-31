4bd0b5d50261d0b5c79d7879a449ff5cdc30d964
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-01-31 05:25:31 +0300

    updates for modal.js-html-css
    updates for modal.js-html-css

--- src/js/pop_up_exercise.js
+++ src/js/pop_up_exercise.js
@@ -1,58 +1,61 @@
-document.addEventListener("DOMContentLoaded", function () {
+import axios from 'axios';
+import { capitalize } from './capitalize_word';
+
+export function assignModal() {
   // Находим контейнер с классом "exercises-list"
-  var container = document.querySelector(".exercises-list");
-
+  let container = document.querySelector(".exercises-list");
   // Навешиваем обработчик клика на родительский элемент, делегируем событие детям
-  container.addEventListener("click", function(event) {
-    var target = event.target;
-
-    // Проверяем, что кликнули по кнопке ".exercises-start-button"
-    if (target.classList.contains("exercises-start-button")) {
+  container.addEventListener("click", async function (event) {
+    let target = event.target;
+    // Проверяем, что кликнули по кнопке ".exercises-start-button" или на иконку
+    if (target.classList.contains("exercises-start-button") || target.parentElement.classList.contains("exercises-start-button")) {
       // Получаем значение ID из атрибута data-id кнопки
-      var exerciseId = target.getAttribute("data-id");
-
-      
-
+      let exerciseId;
+      if (target.parentElement.classList.contains("exercises-start-button")) {
+        exerciseId = target.parentElement.getAttribute("id");
+      } else {
+        exerciseId = target.getAttribute("id");
+      }
+      async function getInfoByID() {
+        try {
+          await axios.get('https://energyflow.b.goit.study/api/exercises/' + exerciseId)
+            .then(({ data }) => {
+              // Выводим данные на консоль
+              // console.log(data);
+              // Заполняем модальное окно данными
+              document.getElementById("modalImage").src = data.gifUrl;
+              document.getElementById("modalName").textContent = capitalize(data.name);
+              document.getElementById("modalRating").textContent = data.rating;
+              document.getElementById("modalTarget").textContent = capitalize(data.target);
+              document.getElementById("bodyPart").textContent = capitalize(data.bodyPart);
+              document.getElementById("modalEquipment").textContent = capitalize(data.equipment);
+              document.getElementById("modalPopularity").textContent = data.popularity;
+              document.getElementById("modalBurnedCalories").textContent = data.burnedCalories + " cal / " + data.time + " min";
+              document.getElementById("modalDescription").textContent = data.description;
+              // Открываем модальное окно
+              var modal = document.getElementById("pop-up-exercise");
+              modal.style.display = "flex";
+            })
+        }
+        catch (error) {
+          console.error('Error:', error);
+          alert('Something went wrong, try again');
+        }
+      }
       // Отправляем запрос на сервер с учетом полученного exerciseId
-      fetch('https://energyflow.b.goit.study/api/exercises/?id=' + exerciseId)
-        .then(response => {
-          
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          // Выводим данные на консоль
-          console.log(data);
-
-          // Заполняем модальное окно данными
-          document.getElementById("modalTitle").textContent = data.name;
-          document.getElementById("modalImage").src = data.gifUrl;
-          document.getElementById("modalName").textContent = data.name;
-          document.getElementById("modalRating").textContent = data.rating;
-          document.getElementById("modalTarget").textContent = data.bodyPart;
-          document.getElementById("modalEquipment").textContent = data.equipment;
-          document.getElementById("modalPopularity").textContent = data.popularity;
-          document.getElementById("modalBurnedCalories").textContent = data.burnedCalories + " cal / " + data.time + " min";
-          document.getElementById("modalDescription").textContent = data.description;
-
-          // Открываем модальное окно
-          var modal = document.getElementById("pop-up-exercise");
-          modal.style.display = "flex";
-        })
-        .catch(error => {
-          console.error('There was a problem with your fetch operation:', error);
-        });
+      await getInfoByID();
     }
   });
 
   // Навешиваем обработчик события на кнопку закрытия модального окна
-  var closeButton = document.getElementById("closeBtn");
-  closeButton.addEventListener("click", function() {
-    var modal = document.getElementById("pop-up-exercise");
+  const closeButton = document.getElementById("closeBtn");
+  closeButton.addEventListener("click", function () {
+    const modal = document.getElementById("pop-up-exercise");
     modal.style.display = "none";
   });
-});
+}
 
 
+export function deleteListener(params) {
+
+}
