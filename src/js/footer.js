3184dad8ead409024f327a545872aa477d273281
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-02-01 00:12:02 +0300

    Exercises Done - SearchInput Done

--- src/js/footer.js
+++ src/js/footer.js
@@ -9,8 +9,8 @@ form.addEventListener('submit', async function (event) {
         if (pattern.test(email.value)) {
             // dsasd@gmail.com
             await axios.post('https://energyflow.b.goit.study/api/subscription', { email: email.value })
-                .then(response => { console.log(response.data.message) })
-                .catch(error => { console.log(error.response.data.message) });
+                .then(response => { alert(response.data.message) })
+                .catch(error => { alert(error.response.data.message) });
         } else {
             alert('Please enter the correct email');
         }
--- src/js/search.js
+++ src/js/search.js
@@ -1,111 +1,112 @@
-import axios from "axios";
+// import axios from "axios";
+// const form = document.getElementById("exercises-search-form");
+// const searchInput = form.querySelector(".exercises-search-input");
+// const exercisesList = document.querySelector(".exercises-list");
+// const clearButton = document.querySelector('.exercises-inputclear-icon');
+// console.dir(clearButton);
 
 
-const form = document.getElementById("exercises-search-form");
-const searchInput = form.querySelector(".exercises-search-input");
-const exercisesList = document.querySelector(".exercises-list");
-const clearButton = document.querySelector('.exercises-inputclear-icon');
+// form.addEventListener("submit", async (event) => {
+//   event.preventDefault();
 
+//   const query = searchInput.value.trim().toLowerCase();
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  const query = searchInput.value.trim().toLowerCase();
-
-  try {
-    await renderExercises(query);
-  } catch (error) {
-    console.error(error);
-  }
-});
+//   try {
+//     await renderExercises(query);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 //  Додаємо обробник події для введення тексту у полі пошуку
-searchInput.addEventListener('input', handleInputChange);
+// searchInput.addEventListener('input', handleInputChange);
 
 
 // / Функція для відображення або приховування іконки очищення
-function handleInputChange() {
-  const inputValue = searchInput.value.trim();
-  if (inputValue !== '') {
-    clearButton.classList.remove('exercises-is-hidden');
-  } else {
-    clearButton.classList.add('exercises-is-hidden');
-  }
-}
+// function handleInputChange() {
+//   const inputValue = searchInput.value.trim();
+//   if (inputValue !== '') {
+//     clearButton.classList.remove('exercises-is-hidden');
+//   } else {
+//     clearButton.classList.add('exercises-is-hidden');
+//   }
+// }
 // / Функція для очищення input 
-function clearInput() {
-  const searchInput = document.getElementById('exercises-search-input');
-  if (searchInput) {
-    searchInput.value = '';
-  }
-};
+// function clearInput() {
+//   const searchInput = document.getElementById('exercises-search-input');
+//   if (searchInput) {
+//     console.log('True');
 
-clearButton.addEventListener('click', clearInput);
+//     searchInput.value = '';
+//   }
+// };
 
-async function getExercises(query) {
-  try {
-    const response = await axios.get(`https://energyflow.b.goit.study/api/exercises?bodypart=&muscles=abs&equipment=&keyword=${query}&page=1&limit=10`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching exercises:', error);
-    throw error;
-  }
-}
+// clearButton.addEventListener('click', clearInput);
 
-async function renderExercises(query) {
-  try {
-    const exercises = await getExercises(query);
+// async function getExercises(query) {
+//   try {
+//     const response = await axios.get(`https://energyflow.b.goit.study/api/exercises?bodypart=&muscles=abs&equipment=&keyword=${query}&page=1&limit=10`);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching exercises:', error);
+//     throw error;
+//   }
+// }
 
-    exercisesList.innerHTML = '';
+// async function renderExercises(query) {
+//   try {
+//     const exercises = await getExercises(query);
 
-    if (exercises.results.length > 0) {
-      const exercisesHTML = exercises.results.map((exercise) => `
-        <li class="exercises-item-page2">
-          <div class="exercises-card">
-            <div class="exercises-card-top">
-              <div class="exercises-kind-wrapper">
-                <p class="exercises-card-kind">WORKOUT</p>
-                <div class="exercises-card-rating">
-                  <p class="exercises-rating-value">${exercise.rating}</p>
-                  <svg class="exercises-star-icon" width="18" height="18" aria-label="star icon">
-                    <use href="./img/exercises/exercises-sprite.svg#icon-star"></use>
-                  </svg>
-                </div>
-              </div>
-              <button class="exercises-start-button">
-                Start
-                <svg class="exercises-start-icon" width="14" height="14">
-                  <use href="./img/exercises/exercises-sprite.svg#icon-arrow"></use>
-                </svg>
-              </button>
-            </div>
-            <div class="exercises-card-namewrapper">
-              <svg class="exercises-star-icon" width="24" height="24" aria-label="star icon">
-                <use href="./img/exercises/exercises-sprite.svg#icon-icon"></use>
-              </svg>
-              <p class="exercises-card-exname">${exercise.name}</p>
-            </div>
-            <ul class="exercises-card-info">
-              <li class="exercises-info-data">
-                <span class="exercises-data-name">Burned calories: </span>${exercise.calories} / ${exercise.duration} min
-              </li>
-              <li class="exercises-info-data">
-                <span class="exercises-data-name">Body part: </span>${exercise.bodyPart}
-              </li>
-              <li class="exercises-info-data">
-                <span class="exercises-data-name">Target: </span>${exercise.target}
-              </li>
-            </ul>
-          </div>
-        </li>
-      `).join('');
+//     exercisesList.innerHTML = '';
 
-      exercisesList.innerHTML = exercisesHTML;
-    } else {
-      exercisesList.innerHTML = '<p class="no-results-message">Unfortunately, <span class="error-message">no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>';
-    }
-  } catch (error) {
-    console.error(error);
-  }
+//     if (exercises.results.length > 0) {
+//       const exercisesHTML = exercises.results.map((exercise) => `
+//         <li class="exercises-item-page2">
+//           <div class="exercises-card">
+//             <div class="exercises-card-top">
+//               <div class="exercises-kind-wrapper">
+//                 <p class="exercises-card-kind">WORKOUT</p>
+//                 <div class="exercises-card-rating">
+//                   <p class="exercises-rating-value">${exercise.rating}</p>
+//                   <svg class="exercises-star-icon" width="18" height="18" aria-label="star icon">
+//                     <use href="./img/exercises/exercises-sprite.svg#icon-star"></use>
+//                   </svg>
+//                 </div>
+//               </div>
+//               <button class="exercises-start-button">
+//                 Start
+//                 <svg class="exercises-start-icon" width="14" height="14">
+//                   <use href="./img/exercises/exercises-sprite.svg#icon-arrow"></use>
+//                 </svg>
+//               </button>
+//             </div>
+//             <div class="exercises-card-namewrapper">
+//               <svg class="exercises-star-icon" width="24" height="24" aria-label="star icon">
+//                 <use href="./img/exercises/exercises-sprite.svg#icon-icon"></use>
+//               </svg>
+//               <p class="exercises-card-exname">${exercise.name}</p>
+//             </div>
+//             <ul class="exercises-card-info">
+//               <li class="exercises-info-data">
+//                 <span class="exercises-data-name">Burned calories: </span>${exercise.calories} / ${exercise.duration} min
+//               </li>
+//               <li class="exercises-info-data">
+//                 <span class="exercises-data-name">Body part: </span>${exercise.bodyPart}
+//               </li>
+//               <li class="exercises-info-data">
+//                 <span class="exercises-data-name">Target: </span>${exercise.target}
+//               </li>
+//             </ul>
+//           </div>
+//         </li>
+//       `).join('');
+
+//       exercisesList.innerHTML = exercisesHTML;
+//     } else {
+//       exercisesList.innerHTML = '<p class="no-results-message">Unfortunately, <span class="error-message">no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>';
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
 
 
-}
+// }
